1a2ebcf60be8ad21be76fef12446bea1dee40d67
Yulia Zaripova <zaripovaur@.gmail.com>
2023-01-16 22:13:08 +0400

    refactor: configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,18 @@ module.exports = {
   target: 'node',
   entry: './src/index.ts',
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
